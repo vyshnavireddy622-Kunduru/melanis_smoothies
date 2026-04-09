ab558ee440e3bd5d9e63cdc08d2cb80a495c73c2
vyshnavireddy622-Kunduru <vyshnavireddy622@gmail.com>
2026-04-09 11:52:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,4 @@ except Exception as e:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
