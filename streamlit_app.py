23dc915abe2f5ec4cba8efc65de8b45fcdf95b1f
vyshnavireddy622-Kunduru <vyshnavireddy622@gmail.com>
2026-04-09 13:00:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ try:
         ORDER BY FRUIT_NAME
     """).to_pandas()
 
-    pd_df = my_dataframe
+    pd_df = my_dataframe.to_pandas()
+    #st.dataframe(pd_df)
+    #st.stop()
 
     ingredients_list = st.multiselect(
         "Choose up to 5 ingredients:",
